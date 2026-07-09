6ad9e00fd458647d12fb36b9752b7607e5569b08
Zhan Hodzhabergenov <zhan.ernesuly2@gmail.com>
2026-07-09 17:54:38 +0500

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard af02f2286e3a9c18005857e7444239c7e25051c9 Merge pull request #55 from Hong-forever/master

--- ysyx-workbench/npc/csrc/main.cpp
+++ ysyx-workbench/npc/csrc/main.cpp
@@ -1,6 +1,37 @@
+#include <verilated.h>
+#include <verilated_fst_c.h>   // NEW: needed for FST tracing
+#include "Vexample.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    contextp->traceEverOn(true);   // NEW: tell the context tracing will be used
+
+    Vexample* example = new Vexample{contextp};
+
+    VerilatedFstC* tfp = new VerilatedFstC;   // NEW: the trace object
+    example->trace(tfp, 5);                        // NEW: register example's signals, depth 5
+    tfp->open("wave.fst");                      // NEW: output file
+
+    vluint64_t time = 0;   // NEW: tracks simulation time for the trace
+
+    for (int i = 0; i < 30; i++) {   // CHANGED: bounded loop, not while(1)
+        int a = rand() & 1;
+        int b = rand() & 1;
+        example->a = a;
+        example->b = b;
+        example->eval();
+        tfp->dump(time);   // NEW: record this timestep
+        time++;
+        printf("a = %d, b = %d, f = %d\n", a, b, example->f);
+        assert(example->f == (a ^ b));
+    }
+
+    tfp->close();   // NEW: flush/close the trace file
+    example->final();
+    delete example;
+    return 0;
 }
