c686b3ea3ce32bdb8afc33a7ba74bdf3d3b93dad
Zhan Hodzhabergenov <zhan.ernesuly2@gmail.com>
2026-07-13 20:32:44 +0500

    PA2.2 (partial): implement klib string.c and stdio.c
    - string.c: strlen, strcpy, strncpy, strcat, strcmp, strncmp, memset,
      memmove, memcpy, memcmp -- all 10 functions
    - stdio.c: vsprintf (%s and %d only, per handout scope), sprintf, printf,
      vsnprintf, snprintf
    - 35/35 cpu-tests now passing (string and hello-str were the last 2
      blocked on klib -- both now pass)

--- ysyx-workbench/abstract-machine/klib/src/stdio.c
+++ ysyx-workbench/abstract-machine/klib/src/stdio.c
@@ -5,24 +5,84 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *start = out;
+  for (const char *p = fmt; *p != '\0'; p++) {
+    if (*p != '%') {
+      *out++ = *p;
+      continue;
+    }
+    p++; // skip past '%', now look at the format character
+    if (*p == 's') {
+      char *s = va_arg(ap, char *);
+      while (*s != '\0') {
+        *out++ = *s++;
+      }
+    } else if (*p == 'd') {
+      int val = va_arg(ap, int);
+      char buf[12]; // enough digits for a 32-bit int plus a sign
+      int i = 0;
+      int neg = (val < 0);
+      unsigned int uval = neg ? (unsigned int)(-(val + 1)) + 1 : (unsigned int)val;
+      // (the -(val+1)+1 trick avoids overflow if val is INT_MIN, whose
+      // magnitude doesn't fit in a positive int)
+      if (uval == 0) {
+        buf[i++] = '0';
+      } else {
+        while (uval != 0) {
+          buf[i++] = '0' + (uval % 10);
+          uval /= 10;
+        }
+      }
+      if (neg) buf[i++] = '-';
+      while (i > 0) {
+        *out++ = buf[--i]; // digits were built in reverse; write them back out reversed
+      }
+    } else if (*p == '%') {
+      *out++ = '%';
+    }
+  }
+  *out = '\0';
+  return out - start;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...) {
+  char buf[256];
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(buf, fmt, ap);
+  va_end(ap);
+  for (int i = 0; i < ret; i++) {
+    putch(buf[i]);
+  }
+  return ret;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char buf[256];
+  int ret = vsprintf(buf, fmt, ap);
+  size_t i;
+  for (i = 0; i + 1 < n && i < (size_t)ret; i++) {
+    out[i] = buf[i];
+  }
+  if (n > 0) out[i] = '\0';
+  return ret;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 #endif
--- ysyx-workbench/abstract-machine/klib/src/string.c
+++ ysyx-workbench/abstract-machine/klib/src/string.c
@@ -5,43 +5,106 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  for (; *s != '\0'; s++) {
+    len++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;
+  while ((*dst++ = *src++) != '\0')
+    ;
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *ret = dst;
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++) {
+    dst[i] = src[i];
+  }
+  for (; i < n; i++) {
+    dst[i] = '\0';
+  }
+  return ret;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *ret = dst;
+  while (*dst != '\0')
+    dst++;
+  while ((*dst++ = *src++) != '\0')
+    ;
+  return ret;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while (*s1 != '\0' && *s2 != '\0') {
+    if (*s1 != *s2) {
+      return (unsigned char)*s1 - (unsigned char)*s2;
+    }
+    s1++;
+    s2++;
+  }
+  return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  size_t i;
+  for (i = 0; i < n; i++) {
+    if (s1[i] != s2[i]) {
+      return (unsigned char)s1[i] - (unsigned char)s2[i];
+    }
+    if (s1[i] == '\0') {
+      return 0;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  char *p = (char *)s;
+  for (size_t i = 0; i < n; i++) {
+    p[i] = (char)c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  char *d = (char *)dst;
+  const char *s = (const char *)src;
+  if (d < s) {
+    for (size_t i = 0; i < n; i++) {
+      d[i] = s[i];
+    }
+  } else if (d > s) {
+    for (size_t i = n; i > 0; i--) {
+      d[i - 1] = s[i - 1];
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *d = (char *)out;
+  const char *s = (const char *)in;
+  for (size_t i = 0; i < n; i++) {
+    d[i] = s[i];
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const char *p1 = (const char *)s1;
+  const char *p2 = (const char *)s2;
+  for (size_t i = 0; i < n; i++) {
+    if (p1[i] != p2[i]) {
+      return p1[i] - p2[i];
+    }
+  }
+  return 0;
 }
 
 #endif
